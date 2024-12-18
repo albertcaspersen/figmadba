4702e99f2d033cfb5fe5e8c6b39e88286d607371
Jovoje <josefine_voge@hotmail.com>
2024-12-18 12:44:52 +0100

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="hero">
         <p class="title">Glas keramik skulptur</p>
+        <p class="price">1.399 kr.</p>
+        <hr>
         <img src="./assets/Fisk.png" alt="" class="fisk">
     </div>
 </template>
@@ -9,13 +11,29 @@
     .hero {
     display: grid;
     grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(3, 40px);
     width: 100%;
     
   }
   
   .title {
     grid-column: 2;
+    grid-row: 1;
     justify-content: start;
+    color: black;
+    font-size: 32px;
+  }
+
+  .price {
+    grid-row: 2;
+    color: #00098A;
+    grid-column: 2;
+    font-family: "Plus Jakarta Sans";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 1.6px;
   }
 
   .fisk {
@@ -23,9 +41,20 @@
     border-radius: 20px;
     width: 500px;
     height: 375.803px;
+    margin-top: 30px;
     flex-shrink: 0;
-
+    grid-row: 4;
   }
+
+  hr {
+  grid-row: 4;
+  width: 500px;
+  height: 0px;
+  stroke: blue;
+  stroke-linecap: round;
+  grid-column:2;
+  }
+
 </style>
 
 <script>
