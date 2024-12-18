535c677ea87c7f779da4b08abf319a7a12400bba
Jovoje <josefine_voge@hotmail.com>
2024-12-18 11:18:03 +0100

    logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import Button from './components/Button.vue'
 import Searchbar from './components/Searchbar.vue'
+import Logo from './components/icons/logo.png'
 </script>
 
 <template>
 
   <main>
     <div class="header">
-      <h1>Hello world</h1>
+      <img src="./components/icons/logo.png" alt="" class="logo">
       <Searchbar></Searchbar>
       <Button type="primary" class="button">Opret annonce</Button>
       <Button type="tetriary" class="button2">Log ind</Button>
@@ -23,12 +24,28 @@ import Searchbar from './components/Searchbar.vue'
   height: 138px; /* Fixed height */
   gap: 16px; /* Optional spacing between grid items */
   align-items: center; /* Center content vertically */
-  padding: 0 20px; /* Optional padding */
+  padding: 0 0px; /* Optional padding */
+  }
+
+  .logo {
+    grid-column: 1;
+    justify-self: start;
+    width: 250px;
+  height: 131px;
+  flex-shrink: 0; 
   }
  
   .button {
   grid-column: 5; /* Places the button in column 5 */
   justify-self: start; /* Aligns the button to the start of the column */
+  display: flex;
+  width: 200px;
+  height: 50px;
+  padding: 9px 15px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
 }
 
 .button2 {
