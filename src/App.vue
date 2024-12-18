ee782a20b9e1203c0cff5de4c563a63f14424fdb
Jovoje <josefine_voge@hotmail.com>
2024-12-18 11:04:23 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <script setup>
 import Button from './components/Button.vue'
+import Searchbar from './components/Searchbar.vue'
 </script>
 
 <template>
 
   <main>
-    <div>
+    <div class="header">
       <h1>Hello world</h1>
-      <Button>Click</Button>
+      <Searchbar></Searchbar>
+      <Button type="primary" class="button">Opret annonce</Button>
+      <Button type="tetriary" class="button2">Log ind</Button>
+
     </div>
   </main>
 </template>
 
 <style scoped>
+  .header {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr); /* 6 equal columns */
+  height: 138px; /* Fixed height */
+  gap: 16px; /* Optional spacing between grid items */
+  align-items: center; /* Center content vertically */
+  padding: 0 20px; /* Optional padding */
+  }
+ 
+  .button {
+  grid-column: 5; /* Places the button in column 5 */
+  justify-self: start; /* Aligns the button to the start of the column */
+}
+
+.button2 {
+  grid-column: 6;
+  justify-self: start;
+}
+
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,36 @@
+    <template>
+        <div class="searchbar">
+            <p>Søg på DBA</p>
+        </div>
+    </template>
+
+<script>
+export default {
+  name: "Searchbar",
+};
+
+</script>
+
+    <style>
+    .searchbar {
+        width: 500px;
+        height: 50px;
+        border-radius: 28px;
+        border: 3px solid #101279;
+        background: #FFF;
+    }
+
+    p {
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 125.987%; /* 25.197px */
+        letter-spacing: 1px;
+        color: #ACACAC;
+        width: 145.918px;
+        height: 35px;
+
+
+    }
+
+    </style>
